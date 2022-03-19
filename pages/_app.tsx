9f9b5d773edb1698ddd248cb6687e8210053aa36
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-03-18 21:51:52 -0700

    feat(pages): Set up MUI on main Next pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,41 @@
-import type { AppProps } from 'next/app'
+/**
+ * The setup for this page is based on the official mui-next example.
+ * It is used under MIT license (with same license notice as mui).
+ * @see https://github.com/mui/material-ui/blob/master/examples/nextjs/pages/_app.js
+ */
+import type PropTypes from "prop-types";
+import type { EmotionCache } from "@emotion/cache";
+import Head from "next/head";
+import { ThemeProvider, StyledEngineProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import theme from "themes/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface Props {
+  Component: PropTypes.ReactComponentLike;
+  pageProps: object;
 }
 
-export default MyApp
+const cache: EmotionCache = createCache({ key: "css", prepend: true });
+cache.compat = true;
+
+function MyApp(props: Props) {
+  const { Component, pageProps } = props;
+  return (
+    <CacheProvider value={cache}>
+      <Head>
+        <title>Voxeldesk</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </CacheProvider>
+  );
+}
+
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,26 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import VoxelCanvas from "../components/canvas/VoxelCanvas";
 
-const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
-        ></meta>
-        <title>Voxeldesk</title>
-        <meta
-          name="description"
-          content="Create voxel art online by clicking on a grid."
-        />
-      </Head>
-      <VoxelCanvas />
-    </>
-  );
-};
+const PageHead = (): JSX.Element => (
+  <Head>
+    <meta charSet="utf-8" />
+    <meta
+      name="viewport"
+      content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
+    />
+    <title>Voxeldesk</title>
+    <meta
+      name="description"
+      content="Create voxel art online by clicking on a grid."
+    />
+  </Head>
+);
+
+const Home: NextPage = () => (
+  <>
+    <PageHead />
+    <VoxelCanvas />
+  </>
+);
 
 export default Home;
