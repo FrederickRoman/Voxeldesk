40e4d3c983e277c377917259120ba7c4872b2c0d
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-03-18 18:55:35 -0700

    refactor(index): Remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
 import VoxelCanvas from "../components/canvas/VoxelCanvas";
 
 const Home: NextPage = () => {
