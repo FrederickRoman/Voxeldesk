d4158da3904396c612d6900545d31a8f54dcdc3e
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-03-21 21:17:55 -0700

    refactor(index): Use path alias for import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import VoxelCanvas from "../components/canvas/VoxelCanvas";
+import VoxelCanvas from "components/editor/canvas/VoxelCanvas";
 
 const PageHead = (): JSX.Element => (
   <Head>
