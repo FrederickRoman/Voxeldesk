d84b12a779ddcfc3c387564ef3744c1298503e0f
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-03-26 01:19:59 -0700

    feat(ColorPicker): Replace outlined close Button with Close IconButton

--- components/editor/actions/color/ColorPicker.tsx
+++ components/editor/actions/color/ColorPicker.tsx
@@ -1,4 +1,5 @@
-import { Box, Button, Grid, Paper } from "@mui/material";
+import { Box, Grid, IconButton, Paper } from "@mui/material";
+import { Close } from "@mui/icons-material";
 import { HexColorPicker } from "react-colorful";
 import type { Color } from "three";
 
@@ -13,7 +14,17 @@ function ColorPicker(props: Props): JSX.Element {
   const { color, colorsUsed, handleColorChange, handleCloseOption } = props;
 
   return (
-    <Paper elevation={3}>
+    <Box component={Paper} elevation={3} position="relative">
+      <Box position="absolute" top={3} right={3}>
+        <IconButton
+          aria-label="close"
+          size="small"
+          sx={{ color: "primary.light", "&:hover": { color: "primary.main" } }}
+          onClick={handleCloseOption}
+        >
+          <Close />
+        </IconButton>
+      </Box>
       <Grid
         container
         flexDirection="column"
@@ -29,42 +40,34 @@ function ColorPicker(props: Props): JSX.Element {
         <Grid item>
           <HexColorPicker color={color} onChange={handleColorChange} />
         </Grid>
-        <Grid item>
-          <Grid
-            container
-            width={200}
-            my={1}
-            justifyContent="space-evenly"
-            alignItems="center"
-          >
-            {colorsUsed
-              .map((colorUsed) => `#${colorUsed.getHexString()}`)
-              .map((colorUsedHexString, i) => (
-                <Grid item key={i}>
-                  <Box
-                    height={20}
-                    width={20}
-                    m={1}
-                    border="1px solid black"
-                    sx={{ background: colorUsedHexString }}
-                    onClick={() => handleColorChange(colorUsedHexString)}
-                  />
-                </Grid>
-              ))}
+        {colorsUsed.length > 0 && (
+          <Grid item>
+            <Grid
+              container
+              width={200}
+              my={1}
+              justifyContent="space-evenly"
+              alignItems="center"
+            >
+              {colorsUsed
+                .map((colorUsed) => `#${colorUsed.getHexString()}`)
+                .map((colorUsedHexString, i) => (
+                  <Grid item key={i}>
+                    <Box
+                      height={20}
+                      width={20}
+                      m={1}
+                      border="1px solid black"
+                      sx={{ background: colorUsedHexString }}
+                      onClick={() => handleColorChange(colorUsedHexString)}
+                    />
+                  </Grid>
+                ))}
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid item>
-          <Box
-            component={Button}
-            variant="outlined"
-            my={2}
-            onClick={handleCloseOption}
-          >
-            Close
-          </Box>
-        </Grid>
+        )}
       </Grid>
-    </Paper>
+    </Box>
   );
 }
 
