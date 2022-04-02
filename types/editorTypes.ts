3eb7e254991109adb4b2e3438b4c85b7ddd8877b
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-04-01 17:42:51 -0700

    feat(editorTypes): Add Action type "Add/Remove"

--- types/editorTypes.ts
+++ types/editorTypes.ts
@@ -1,7 +1,7 @@
 import type { Mesh, BoxGeometry, MeshLambertMaterial, Color } from "three";
 
 export type Voxel = Mesh<BoxGeometry, MeshLambertMaterial>;
-export type Action = "Color" | "Undo" | "Reset" | "Save" | "";
+export type Action = "Add/Remove" | "Color" | "Undo" | "Reset" | "Save" | "";
 
 export interface VoxelTopology {
   vertices: { x: number; y: number; z: number }[];
