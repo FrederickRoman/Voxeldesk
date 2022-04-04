68679e75e925fce72083487bba799eba7b515f1e
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-04-03 22:45:44 -0700

    feat(EventBus): Use event-target-shim for event bus

--- services/EventBus.ts
+++ services/EventBus.ts
@@ -1,13 +1,18 @@
-class EventBus extends EventTarget {
+import { EventTarget as EventTargetShim } from "event-target-shim";
+import type { Event as EventShim } from "event-target-shim";
+
+const eventBusNode = new EventTargetShim();
+
+class EventBus {
   public on(type: string, listener: (event: Event) => void) {
-    this.addEventListener(type, listener);
+    eventBusNode.addEventListener(type, listener);
   }
   public off(type: string, listener: (event: Event) => void) {
-    this.removeEventListener(type, listener);
+    eventBusNode.removeEventListener(type, listener);
   }
   public emit(type: string, data: any) {
     const customEvent = new CustomEvent(type, { detail: data });
-    this.dispatchEvent(customEvent);
+    eventBusNode.dispatchEvent(customEvent as EventShim);
   }
 }
 
