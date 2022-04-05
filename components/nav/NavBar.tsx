ac4bb60b34e2d823e094a028ef255e41d0261ed8
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-04-04 21:28:07 -0700

    feat(NavBar): Add logo Image to NavBar's HomeLink

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -1,56 +1,59 @@
 import {
-    AppBar,
-    Box,
-    Button,
-    Grid,
-    IconButton,
-    Toolbar,
-    Typography,
-  } from "@mui/material";
-  import HelpIcon from "@mui/icons-material/Help";
-  
-  function HomeLink(): JSX.Element {
-    return (
-      <Box>
-        <Button>
-          <Grid container justifyContent="center" alignItems="center">
-            <Grid item>{/* Insert logo image here */}</Grid>
-            <Grid item>
-              <Typography variant="h6" sx={{ color: "background.default" }}>
-                Voxeldesk
-              </Typography>
-            </Grid>
+  AppBar,
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import HelpIcon from "@mui/icons-material/Help";
+import Image from "next/image";
+
+function HomeLink(): JSX.Element {
+  return (
+    <Box>
+      <Button>
+        <Grid container justifyContent="center" alignItems="center">
+          <Grid item sx={{ display: "grid", placeContent:"center", mx:1}}>
+            <Image src="/img/voxeldesk_logo.png" width={30} height={30}/>
           </Grid>
-        </Button>
-      </Box>
-    );
-  }
-  
-  function AboutLink(): JSX.Element {
-    return (
-      <Box>
-        <IconButton size="large" aria-label="about">
-          <HelpIcon sx={{ color: "background.default" }} />
-        </IconButton>
-      </Box>
-    );
-  }
-  
-  function NavBar(): JSX.Element {
-    return (
-      <AppBar position="static">
-        <Toolbar>
-          <Grid container justifyContent="space-between" alignItems="center">
-            <Grid item>
-              <HomeLink />
-            </Grid>
-            <Grid item>
-              <AboutLink />
-            </Grid>
+          <Grid item>
+            <Typography variant="h6" sx={{ color: "background.default" }}>
+              Voxeldesk
+            </Typography>
           </Grid>
-        </Toolbar>
-      </AppBar>
-    );
-  }
-  
-  export default NavBar;
+        </Grid>
+      </Button>
+    </Box>
+  );
+}
+
+function AboutLink(): JSX.Element {
+  return (
+    <Box>
+      <IconButton size="large" aria-label="about">
+        <HelpIcon sx={{ color: "background.default" }} />
+      </IconButton>
+    </Box>
+  );
+}
+
+function NavBar(): JSX.Element {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Grid container justifyContent="space-between" alignItems="center">
+          <Grid item>
+            <HomeLink />
+          </Grid>
+          <Grid item>
+            <AboutLink />
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default NavBar;
