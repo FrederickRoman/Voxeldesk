b439609bcafb4764a9b704b294b38da0c993f40f
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-04-19 15:38:47 -0700

    feat(editorTypes): Add Load string literal to Action type

--- types/editorTypes.ts
+++ types/editorTypes.ts
@@ -1,7 +1,14 @@
 import type { Mesh, BoxGeometry, MeshLambertMaterial, Color } from "three";
 
 export type Voxel = Mesh<BoxGeometry, MeshLambertMaterial>;
-export type Action = "Add/Remove" | "Color" | "Undo" | "Reset" | "Save" | "";
+export type Action =
+  | "Add/Remove"
+  | "Color"
+  | "Undo"
+  | "Reset"
+  | "Save"
+  | "Load"
+  | "";
 export type EditMode = "add" | "remove";
 
 export interface VoxelTopology {
