3027a2b3baa8a998190360e2bd0fd423815dfee8
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-04-20 23:25:00 -0700

    refactor(VoxelWorld): Move public fields and methods before private ones

--- services/world/VoxelWorld.ts
+++ services/world/VoxelWorld.ts
@@ -28,6 +28,15 @@ import type {
 import EventBus from "services/bus/EventBus";
 
 class VoxelWorld {
+  public eventBus = new EventBus();
+  private readonly DEFAULT_VOXEL_LENGTH = 50;
+  private readonly DEFAULT_VOXEL_COLOR = 0xfeb74c;
+  private readonly DEFAULT_VOXEL_GEOMETRY = new BoxGeometry(
+    ...new Array(3).fill(this.DEFAULT_VOXEL_LENGTH)
+  );
+  private readonly DEFAULT_VOXEL_MATERIAL = new MeshLambertMaterial({
+    color: this.DEFAULT_VOXEL_COLOR,
+  });
   private camera!: PerspectiveCamera;
   private scene!: Scene;
   private renderer!: WebGLRenderer;
@@ -35,27 +44,22 @@ class VoxelWorld {
   private pointer = new Vector2();
   private rollOverMesh!: Mesh;
   private objects: (Voxel | Object3D<THREE.Event>)[] = [];
-  private isMouseDown = false;
+  private isMouseDown: boolean = false;
   private onMouseDownPosition = new Vector2();
-  private theta = 45;
-  private onMouseDownTheta = 45;
-  private phi = 60;
-  private onMouseDownPhi = 60;
+  private theta: number = 45;
+  private onMouseDownTheta: number = 45;
+  private phi: number = 60;
+  private onMouseDownPhi: number = 60;
   private plane!: Mesh;
-  private pickedColor = new Color(0xfeb74c);
+  private pickedColor = new Color(this.DEFAULT_VOXEL_COLOR);
   private usedColors: Color[] = [];
-  private readonly CUBE_LEN = 50;
-  private readonly cubeDims: number[] = new Array(3).fill(this.CUBE_LEN);
-  private readonly cubeGeo = new BoxGeometry(...this.cubeDims);
-  private cubeMaterial = new MeshLambertMaterial({ color: 0xfeb74c });
   private history: Edit.History = [];
-  public eventBus = new EventBus();
   constructor(canvas: HTMLCanvasElement) {
     this.camera = this.createCamera();
     this.scene = this.createScene();
     this.renderer = this.createRenderer(canvas);
   }
-  public setPickedColor(color: THREE.ColorRepresentation) {
+  public setPickedColor(color: THREE.ColorRepresentation): void {
     this.pickedColor = new Color(color);
   }
   public render(): void {
@@ -88,9 +92,7 @@ class VoxelWorld {
     this.onMouseDownPosition.x = event.touches[0].clientX - left;
     this.onMouseDownPosition.y = event.touches[0].clientY - top;
   }
-  private clipToTopView(phi: number): number {
-    return Math.min(180, Math.max(0, phi));
-  }
+
   public onMouseMove(event: React.MouseEvent): void {
     event.preventDefault();
     if (event.button !== 0) return;
@@ -123,6 +125,30 @@ class VoxelWorld {
       }
     }
     this.render();
+  }
+  public onMouseUp(event: React.MouseEvent, mode: EditMode): void {
+    event.preventDefault();
+    if (event.button !== 0) return;
+    const { top, left } = event.currentTarget.getBoundingClientRect();
+    this.isMouseDown = false;
+    this.onMouseDownPosition.x =
+      event.clientX - left - this.onMouseDownPosition.x;
+    this.onMouseDownPosition.y =
+      event.clientY - top - this.onMouseDownPosition.y;
+    if (this.onMouseDownPosition.length() > 5) return;
+
+    this.pointer.set(
+      ((event.clientX - left) / window.innerWidth) * 2 - 1,
+      -((event.clientY - top) / window.innerHeight) * 2 + 1
+    );
+
+    const intersects = this.checkIntersects();
+    if (intersects.length > 0) {
+      const intersect = intersects[0];
+      if (mode == "add") this.addVoxelAt(intersect);
+      else if (mode == "remove") this.removeVoxelAt(intersect);
+      this.render();
+    }
   }
   public onTouchMove(event: React.TouchEvent): void {
     console.log("onTouchMove");
@@ -160,87 +186,7 @@ class VoxelWorld {
     }
     this.render();
   }
-  private checkIntersects(): Intersection[] {
-    this.raycaster.setFromCamera(this.pointer, this.camera);
-    return this.raycaster.intersectObjects(this.objects, false);
-  }
-  private emitWorldChange(state: string): void {
-    if (state == "usedColors") {
-      if (!this.usedColors.some((color) => color.equals(this.pickedColor)))
-        this.usedColors.push(this.pickedColor);
-      const payload = { usedColors: [...this.usedColors] };
-      this.eventBus.emit("usedColorsChange", payload);
-    }
-  }
 
-  private addVoxelAt(intersect: Intersection<Object3D<THREE.Event>>): void {
-    if (intersect.face) {
-      const cubeMaterial = this.cubeMaterial.clone();
-      cubeMaterial.color = this.pickedColor;
-      const voxel = new Mesh(this.cubeGeo, cubeMaterial);
-      voxel.position
-        .copy(intersect.point)
-        .add(intersect.face.normal)
-        .divideScalar(50)
-        .floor()
-        .multiplyScalar(50)
-        .addScalar(25);
-      this.scene.add(voxel);
-      this.objects.push(voxel);
-      this.history.push({
-        action: "addition",
-        object: {
-          type: "voxel",
-          position: voxel.position,
-          color: voxel.material.color,
-        },
-      });
-      console.log(this.history);
-      this.emitWorldChange("usedColors");
-    }
-  }
-  private removeVoxelAt(intersect: Intersection<Object3D<THREE.Event>>): void {
-    if (intersect.object !== this.plane) {
-      const object = this.objects.find((obj) => obj == intersect.object);
-      if (object && this.isVoxel(object)) {
-        this.scene.remove(intersect.object);
-        this.objects.splice(this.objects.indexOf(intersect.object), 1);
-        this.history.push({
-          action: "removal",
-          object: {
-            type: "voxel",
-            position: intersect.object.position,
-            color: (object as Voxel).material.color,
-          },
-        });
-        console.log(this.history);
-      }
-    }
-  }
-  public onMouseUp(event: React.MouseEvent, mode: EditMode): void {
-    event.preventDefault();
-    if (event.button !== 0) return;
-    const { top, left } = event.currentTarget.getBoundingClientRect();
-    this.isMouseDown = false;
-    this.onMouseDownPosition.x =
-      event.clientX - left - this.onMouseDownPosition.x;
-    this.onMouseDownPosition.y =
-      event.clientY - top - this.onMouseDownPosition.y;
-    if (this.onMouseDownPosition.length() > 5) return;
-
-    this.pointer.set(
-      ((event.clientX - left) / window.innerWidth) * 2 - 1,
-      -((event.clientY - top) / window.innerHeight) * 2 + 1
-    );
-
-    const intersects = this.checkIntersects();
-    if (intersects.length > 0) {
-      const intersect = intersects[0];
-      if (mode == "add") this.addVoxelAt(intersect);
-      else if (mode == "remove") this.removeVoxelAt(intersect);
-      this.render();
-    }
-  }
   public onTouchEnd(event: React.TouchEvent, mode: EditMode): void {
     console.log("onTouchEnd");
     console.log(event);
@@ -295,10 +241,119 @@ class VoxelWorld {
     }
     this.render();
   }
+  public onSave(): Model3d {
+    const NUM_CUBE_VERTICES = 8;
+    const mtlColorSet = new Set<Color>();
+    const model3d = Object.seal({ obj: "", mtl: "" });
+    this.objects
+      .filter((object) => this.isVoxel(object))
+      .map((voxel): Voxel => voxel as Voxel)
+      .map((voxel, i) => this.topologizeVoxel(voxel, NUM_CUBE_VERTICES * i))
+      .forEach(({ vertices, faces, color }) => {
+        const colorName = color.getHex().toString(16);
+        if (!mtlColorSet.has(color)) {
+          model3d.mtl += `newmtl ${colorName}\n`;
+          model3d.mtl += `Kd ${color.r} ${color.g} ${color.b}\n\n`;
+          mtlColorSet.add(color);
+        }
+        model3d.obj += `usemtl ${colorName}\n`;
+        vertices.forEach((vertex) => {
+          model3d.obj += `v ${vertex.x} ${vertex.y} ${vertex.z}\n`;
+        });
+        faces.forEach((face) => {
+          model3d.obj += `f ${face.join(" ")}\n`;
+        });
+      });
+    if (model3d.mtl.length > 0)
+      model3d.obj = "mtllib ./material.mtl\n" + model3d.obj;
+    return model3d;
+  }
+  public onLoadModel(model3d: Model3d): void {
+    try {
+      const VOXEL_NUM_LINES = 15;
+      const OFFSET = this.DEFAULT_VOXEL_LENGTH / 2;
+      const offsetVector = new Vector3(OFFSET, OFFSET, -OFFSET);
+      const objLines = model3d.obj.trim().split(/\r?\n/).slice(1);
+      const voxelsData: { color: Color; position: Vector3 }[] = [];
+      for (let i = 0; i < objLines.length / VOXEL_NUM_LINES; i++) {
+        const colorLine = objLines[i * VOXEL_NUM_LINES].split(/\s/);
+        const vertexLine = objLines[i * VOXEL_NUM_LINES + 1].split(/\s/);
+        const color = new Color(`#${colorLine[1]}` as ColorRepresentation);
+        const [x, y, z] = vertexLine.slice(1).map((v) => Number(v));
+        const position = new Vector3(x, y, z).add(offsetVector);
+        voxelsData.push({ color, position });
+      }
+      voxelsData.forEach(({ color, position }) =>
+        this.addVoxel(color, position)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  private clipToTopView(phi: number): number {
+    return Math.min(180, Math.max(0, phi));
+  }
+  private checkIntersects(): Intersection[] {
+    this.raycaster.setFromCamera(this.pointer, this.camera);
+    return this.raycaster.intersectObjects(this.objects, false);
+  }
+  private emitWorldChange(state: string): void {
+    if (state == "usedColors") {
+      if (!this.usedColors.some((color) => color.equals(this.pickedColor)))
+        this.usedColors.push(this.pickedColor);
+      const payload = { usedColors: [...this.usedColors] };
+      this.eventBus.emit("usedColorsChange", payload);
+    }
+  }
+
+  private addVoxelAt(intersect: Intersection<Object3D<THREE.Event>>): void {
+    if (intersect.face) {
+      const cubeMaterial = this.DEFAULT_VOXEL_MATERIAL.clone();
+      cubeMaterial.color = this.pickedColor;
+      const voxel = new Mesh(this.DEFAULT_VOXEL_GEOMETRY, cubeMaterial);
+      voxel.position
+        .copy(intersect.point)
+        .add(intersect.face.normal)
+        .divideScalar(50)
+        .floor()
+        .multiplyScalar(50)
+        .addScalar(25);
+      this.scene.add(voxel);
+      this.objects.push(voxel);
+      this.history.push({
+        action: "addition",
+        object: {
+          type: "voxel",
+          position: voxel.position,
+          color: voxel.material.color,
+        },
+      });
+      console.log(this.history);
+      this.emitWorldChange("usedColors");
+    }
+  }
+  private removeVoxelAt(intersect: Intersection<Object3D<THREE.Event>>): void {
+    if (intersect.object !== this.plane) {
+      const object = this.objects.find((obj) => obj == intersect.object);
+      if (object && this.isVoxel(object)) {
+        this.scene.remove(intersect.object);
+        this.objects.splice(this.objects.indexOf(intersect.object), 1);
+        this.history.push({
+          action: "removal",
+          object: {
+            type: "voxel",
+            position: intersect.object.position,
+            color: (object as Voxel).material.color,
+          },
+        });
+        console.log(this.history);
+      }
+    }
+  }
   private addVoxel(color: Color, position: Vector3): void {
-    const cubeMaterial = this.cubeMaterial.clone();
+    const cubeMaterial = this.DEFAULT_VOXEL_MATERIAL.clone();
     cubeMaterial.color = color;
-    const voxel: Voxel = new Mesh(this.cubeGeo, cubeMaterial);
+    const voxel: Voxel = new Mesh(this.DEFAULT_VOXEL_GEOMETRY, cubeMaterial);
     voxel.position.copy(position);
     this.scene.add(voxel);
     this.objects.push(voxel);
@@ -346,55 +401,7 @@ class VoxelWorld {
       object.material !== undefined
     );
   }
-  public onSave(): Model3d {
-    const NUM_CUBE_VERTICES = 8;
-    const mtlColorSet = new Set<Color>();
-    const model3d = Object.seal({ obj: "", mtl: "" });
-    this.objects
-      .filter((object) => this.isVoxel(object))
-      .map((voxel): Voxel => voxel as Voxel)
-      .map((voxel, i) => this.topologizeVoxel(voxel, NUM_CUBE_VERTICES * i))
-      .forEach(({ vertices, faces, color }) => {
-        const colorName = color.getHex().toString(16);
-        if (!mtlColorSet.has(color)) {
-          model3d.mtl += `newmtl ${colorName}\n`;
-          model3d.mtl += `Kd ${color.r} ${color.g} ${color.b}\n\n`;
-          mtlColorSet.add(color);
-        }
-        model3d.obj += `usemtl ${colorName}\n`;
-        vertices.forEach((vertex) => {
-          model3d.obj += `v ${vertex.x} ${vertex.y} ${vertex.z}\n`;
-        });
-        faces.forEach((face) => {
-          model3d.obj += `f ${face.join(" ")}\n`;
-        });
-      });
-    if (model3d.mtl.length > 0)
-      model3d.obj = "mtllib ./material.mtl\n" + model3d.obj;
-    return model3d;
-  }
-  public onLoadModel(model3d: Model3d): void {
-    try {
-      const VOXEL_NUM_LINES = 15;
-      const OFFSET = this.CUBE_LEN / 2;
-      const offsetVector = new Vector3(OFFSET, OFFSET, -OFFSET);
-      const objLines = model3d.obj.trim().split(/\r?\n/).slice(1);
-      const voxelsData: { color: Color; position: Vector3 }[] = [];
-      for (let i = 0; i < objLines.length / VOXEL_NUM_LINES; i++) {
-        const colorLine = objLines[i * VOXEL_NUM_LINES].split(/\s/);
-        const vertexLine = objLines[i * VOXEL_NUM_LINES + 1].split(/\s/);
-        const color = new Color(`#${colorLine[1]}` as ColorRepresentation);
-        const [x, y, z] = vertexLine.slice(1).map((v) => Number(v));
-        const position = new Vector3(x, y, z).add(offsetVector);
-        voxelsData.push({ color, position });
-      }
-      voxelsData.forEach(({ color, position }) =>
-        this.addVoxel(color, position)
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  }
+
   private orbit(theta: number, phi: number): void {
     const RADIUS = 1600;
     const { sin, cos, PI } = Math;
