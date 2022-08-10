1f5bbe29d1f6df35776f47b889efaf1cd4b4834d
Frederick <11379133+FrederickRoman@users.noreply.github.com>
2022-08-10 00:29:37 -0700

    fix(MainFooter): Fix layout spacing

--- components/footer/MainFooter.tsx
+++ components/footer/MainFooter.tsx
@@ -1,53 +1,54 @@
 import { AppBar, Grid, Link, Toolbar, Typography } from "@mui/material";
 
-function MainFooter(): JSX.Element {
+function CopyrightArea(): JSX.Element {
   const currentYear: number = new Date().getFullYear();
+  return (
+    <Grid container justifyContent="start" alignItems="center">
+      <Grid item>
+        <Link
+          color="inherit"
+          underline="hover"
+          href="https://www.frederickroman.com/"
+        >
+          <Typography variant="body1" color="white" sx={{ mx: 1 }}>
+            © &nbsp; Frederick Roman {currentYear}
+          </Typography>
+        </Link>
+      </Grid>
+      <Grid item>
+        <Link color="inherit" underline="hover" href="https://homeroroman.com/">
+          <Typography variant="body1" color="white" sx={{ mx: 1 }}>
+            © &nbsp; Homero Roman {currentYear}
+          </Typography>
+        </Link>
+      </Grid>
+    </Grid>
+  );
+}
+
+function InfoLinksArea(): JSX.Element {
+  return (
+    <Grid container justifyContent="center" alignItems="center">
+      <Grid item>
+        <Link color="inherit" underline="hover" href="/about">
+          About
+        </Link>
+      </Grid>
+    </Grid>
+  );
+}
+
+function MainFooter(): JSX.Element {
   return (
     <AppBar position="static" sx={{ py: 2 }}>
       <Toolbar>
         <Grid container justifyContent="space-between" alignItems="center">
           <Grid item>
-            <Grid
-              container
-              flexDirection="column"
-              justifyContent="center"
-              alignItems="start"
-            >
-              <Grid item>
-                <Link
-                  color="inherit"
-                  underline="hover"
-                  href="https://www.frederickroman.com/"
-                >
-                  <Typography variant="body1" color="white" align="center">
-                    © &nbsp; Frederick Roman {currentYear} &amp;
-                  </Typography>
-                  <br />
-                </Link>
-              </Grid>
-
-              <Grid item>
-                <Link
-                  color="inherit"
-                  underline="hover"
-                  href="https://homeroroman.com/"
-                >
-                  <Typography variant="body1" color="white" align="center">
-                    © &nbsp; Homero Roman {currentYear}
-                  </Typography>
-                </Link>
-              </Grid>
-            </Grid>
+            <CopyrightArea />
           </Grid>
-          <Grid item>
-            <Link
-              color="inherit"
-              underline="hover"
-              href="/about"
-            >
-              About
-            </Link>
-          </Grid>
+        </Grid>
+        <Grid item>
+          <InfoLinksArea />
         </Grid>
       </Toolbar>
     </AppBar>
